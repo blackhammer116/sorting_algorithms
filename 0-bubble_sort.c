2e2407340624c7f1aeb73d3810a6743c4459ecd3
Nebiyu <nebiyuhiyabu@gmail.com>
2023-02-19 06:09:37 -0800

    using bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,7 +15,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = size - 1; j > i; j--)
 		{
-			if (array[j] < array [j - 1])
+			if (array[j] < array[j - 1])
 			{
 				temp = array[j - 1];
 				array[j - 1] = array[j];
