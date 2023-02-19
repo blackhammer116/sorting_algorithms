1dd7a1e92dcc53a3f7d911bf2994d6542a8baecc
Nebiyu <nebiyuhiyabu@gmail.com>
2023-02-19 06:23:14 -0800

    using bubble sort

--- 0-main.c
+++ 0-main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    int array[] = {190, 728, 668, 260, 815, 61, 171, 964, 715, 152};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
