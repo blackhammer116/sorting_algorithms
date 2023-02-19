946f4ba81c3fbd1ea00f7d1d97bb21968a5ed5d4
Nebiyu <nebiyuhiyabu@gmail.com>
2023-02-19 09:28:47 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,8 +8,8 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, small;
-	int temp, k = 0;
+	size_t i, j, small, k;
+	int temp, sorted;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -22,19 +22,19 @@ void selection_sort(int *array, size_t size)
 		temp = array[small];
 		array[small] = array[i];
 		array[i] = temp;
-		k = 0
+		k = 0;
 		while(k < size)
 		{
 			if (array[k] < array[k + 1])
-				sorted = true;
+				sorted = 1;
 			else
 			{
-				sorted = false;
+				sorted = -1;
 				break;
 			}
 			k++;
 		}
-		if (sorted  == true)
+		if (sorted  == -1)
 			print_array(array, size);
 	}
 }
