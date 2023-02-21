4b2dfc0a825a27a914a96000571968262df61fc1
Nebiyu <nebiyuhiyabu@gmail.com>
2023-02-21 09:17:50 +0300

    using quick sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,24 +7,21 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head = *list;
-	listint_t *tmp, *tmp2 = head->next;
-	size_t i, j;
-	int temp, n1, n2;
-	size_t size = list_size(&head);
+	listint_t *head_ref = *list, *current = *list, *temp = *list->next, *tmp;
 
-	for (i = 1; i < size; i++, tmp->next, tmp->next)
+	if (current->n >= temp->n)
 	{
-		temp = tmp->n;
-		for (j = i; j > 0 && temp < tmp2->prev->n; j--)
-		{
-			n1 = tmp->n;
-			n2 = tmp2->prev->n;
-			n1 = n2;
-			n2 = temp;
-			tmp->n = n1;
-			tmp2->prev->n = n2;
-			print_list(head);
-		}
+		tmp = current;
+		current->next = temp->next;
+		current->prev = temp;
+		temp->next = current;
+		temp->prev = NULL
+	}
+
+	else
+	{
+		while (current->n <= temp->n && current->next != NULL)
+			current = current->next;
+		
 	}
 }
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,57 +1,78 @@
+#include <stdio.h>
 #include "sort.h"
 
-/**
- *
- */
-void swap(int* a,int* b)
-{
-	int t = *a;
-	*a = *b;
-	*b = t;
-}
-/**
- *
- */
-int partition(int array[], int low, int high, size_t size)
-{
-	int pivot = array[high];
-	int i = low - 1, j;
 
-	for (j = low; j <= high - 1; j++)
-	{
-		if (array[j] <= pivot)
-		{
-			i++;
-			swap(&array[i], &array[j]);
-			print_array(array, size);
-		}
-	}
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size);
-	return (i + 1);
-}
-/**
- *
- *
- */
-void quickSort(int *array, int low, int high, size_t size)
-{
-	
-	if (low < high)
-	{
-		int pi = partition(array, low, high, size);
-
-		quickSort(array, low, pi - 1, size);
-		quickSort(array, pi + 1, high, size);
-	}
-}
-
+void quickSort(int *arr, int low, int high, size_t size);
+int partition(int *array, int low, int high, size_t size);
 /**
  *
  */
 void quick_sort(int *array, size_t size)
 {
-	int low = 0, high = size - 1;
+    int high = size - 1, low = 0;
 
-	quickSort(array, low, high, size);
+    quickSort(array, low, high, size);
+    /*
+    if (size < 2)
+        return;
+    pivot = array[high];
+    for (i = 0, j = size - 1;; i++, j--) {
+        while (array[i] < pivot)
+            i++;
+        while (array[j] > pivot)
+            j--;
+        if (i >= j)
+            break;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+	print_array(array, size);
+    }
+    quick_sort(array, i);
+    quick_sort(array + i, size - i);
+    */
+}
+/**
+ *
+ */
+void quickSort(int *arr, int low, int high, size_t size)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi = partition(arr, low, high, size);
+
+        /* Separately sort elements before
+         partition and after partition */
+        quickSort(arr, low, pi - 1, size);
+        quickSort(arr, pi + 1, high, size);
+    }
+}
+/**
+ *
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+    int pivot = array[high], temp;
+    int i = (low - 1), j;
+ 
+    for (j = low; j <= high- 1; j++)
+    {
+        /* If current element is smaller than or
+         equal to pivot*/
+        if (array[j] <= pivot)
+        {
+            i++;    /* increment index of smaller element swap(arr[i], arr[j]); */
+	    temp = array[i];
+	    array[i] = array[j];
+	    array[j] = temp;
+        }
+    }
+    temp = array[i + 1];
+    array[i + 1] = array[high];
+    array[high] = temp;
+    print_array(array, size);
+
+    return (i + 1);
 }
